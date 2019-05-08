1e085259f922273a7455483926bb72c480d41e94
Ev Che <drdeathpanda@yandex.ru>
2019-05-09 01:12:11 +0300

    Закончил с договором офертой

--- contacts/index.php
+++ contacts/index.php
@@ -3,9 +3,18 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Договор-оферта");
 ?>
 <h1>Контакты</h1>
-<p>
-    <b>E-mail:</b> <a href="mailto:info@robofeed.ru">info@robofeed.ru</a><br/>
-    <b>ОРГН:</b> 318505000044563<br/>
-    <b>ИНН:</b> 505078003987
-</p>
+<table class="table table-striped" style="width: auto;">
+    <tr>
+        <th>E-mail:</th>
+        <td><a href="mailto:info@robofeed.ru">info@robofeed.ru</a></td>
+    </tr>
+    <tr>
+        <th>ОРГНИП:</th>
+        <td>318505000044563</td>
+    </tr>
+    <tr>
+        <th>ИНН:</th>
+        <td>505078003987</td>
+    </tr>
+</table>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
