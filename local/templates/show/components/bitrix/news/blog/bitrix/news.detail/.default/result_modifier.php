db3036dfbe8dfaecbf795ead0f3366f6ef55dcc2
Ev Che <drdeathpanda@yandex.ru>
2019-05-27 18:52:25 +0300

    hotfix2

--- local/templates/show/components/bitrix/news/blog/bitrix/news.detail/.default/result_modifier.php
+++ local/templates/show/components/bitrix/news/blog/bitrix/news.detail/.default/result_modifier.php
@@ -8,7 +8,7 @@ if( !empty( $arResult['ID'] ) && !empty( $arResult['IBLOCK_ID'] ) )
         $ar = $rsElem->fetch();
         if( $ar['PREVIEW_PICTURE'] > 0 )
         {
-            $arResult['PREVIEW_PICTURE'] = \CFile::GetPath($ar['PREVIEW_PICTURE']);
+            $arResult['PREVIEW_PICTURE'] = \CFile::ResizeImageGet($ar['PREVIEW_PICTURE'], ['width' => 9999, 'height' => 400], BX_RESIZE_IMAGE_EXACT, false, false, false, 75);
         }
     }
 }
--- local/templates/show/components/bitrix/news/blog/bitrix/news.list/.default/template.php
+++ local/templates/show/components/bitrix/news/blog/bitrix/news.list/.default/template.php
@@ -22,6 +22,7 @@ $this->setFrameMode(true);
                 <?
                 $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
                 $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+                $arItem["PREVIEW_PICTURE"]["SRC"] = \CFile::ResizeImageGet($arItem["PREVIEW_PICTURE"]["ID"], ['width' => 9999, 'height' => 200], BX_RESIZE_IMAGE_EXACT, false, false, false, 75);
                 ?>
                 <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 robofeed-news__item" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
                     <a href="<?=$arItem["DETAIL_PAGE_URL"]?>" class="img" style="background-image: url('<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>');"></a>
